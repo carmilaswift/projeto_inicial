447157a58de25a59f9d69b80ef9e8641ab45845f
Joao lucas Storze santos <joaolucasstorzesantos@MacBook-Air-de-Joao.local>
2026-06-07 12:41:22 -0300

    aula11 - fatiamento de strings e contagem da funçao len

---
+++ iniciante studies/first study/aula11.py
@@ -0,0 +1,13 @@
+# fatiamento de strings = retorna numero de caracteres da variavel
+
+variavel = 'Olá, Mundo!'
+# print(variavel[5]) # retorna a letra 'M' da palavra 'Mundo'
+
+# fatiamento de strings = retorna um pedaço da string
+#print(variavel[4:8])
+
+# função len() = retorna a quantidade de caracteres da string
+#print(len(variavel[4:10]))
+
+# fatiamento contando de 2 em 2
+print(variavel[0:13:2]) # vai retornar 'Oá ud!'
