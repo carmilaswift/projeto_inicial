8e787a63fb814922dd4d12d97e296241f7f1f35f
carmilaswift <contagratuita89@gmail.com>
2026-06-08 23:35:48 -0300

    Remove pastas vazias

---
+++ aula10.py
@@ -0,0 +1,7 @@
+#print("hello world leila")
+
+# criaçao de nomes que retornam em numeros de caracteres
+#print(len(input('Digite um nome: ')))
+
+# criacao de soma de dois valores digitados
+print(int(input('Digite um numero: ')) + int(input('Digite outro valor: ')))
