7d3e26d594797cce0f0518dd4c1751e90d5c7964
carmilaswift <contagratuita89@gmail.com>
2026-06-26 23:35:56 -0300

    exercise exhibit indexes in lists

---
+++ aula30.py
@@ -0,0 +1,14 @@
+# lists on for 
+# list = ['joseph', 'joao', 'lucas', 'world']
+# posicao = list.index('joao' and 'lucas')
+
+# # for nome in list:
+# print(f'posicao é igual a {posicao}')
+
+list = ['joseph', 'joao', 'lucas', 'world']
+indice =  range(len(list))
+
+for indice in indice:
+    print(indice, list[indice], type(list[indice]))
+
+
