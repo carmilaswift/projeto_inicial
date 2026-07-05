ebc3a5f0bf2043c51bb13ac277acfd2fe2a86d44
carmilaswift <contagratuita89@gmail.com>
2026-07-04 23:50:58 -0300

    make fast if and else in one line

---
+++ aula40.py
@@ -0,0 +1,8 @@
+# Tertiary operation (conditional of one line)
+# <value> if <condition> else <value>
+# condiction = 10 == 11
+# variable = 'value' if True else 'other value'
+# print(variable)
+digit = 9 
+new_digit = digit if digit >= 9 else 0
+print(new_digit)
